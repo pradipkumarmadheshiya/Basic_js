c39843b0524b33b89c4507aa34009f020e9ad19d
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2024-11-06 03:04:02 +0530

    loading... added

--- fetching-in-react/src/FetchingComponent.js
+++ fetching-in-react/src/FetchingComponent.js
@@ -3,19 +3,28 @@ import React, {useEffect, useState} from "react";
 function FetchingComponent(){
 
     const [data, setData]=useState([])
+    const [loading, setLoading]=useState(true)
 
     useEffect(()=>{
 
+        setLoading(true)
         const url="https://jsonplaceholder.typicode.com/users"
 
         fetch(url)
             .then(res=>res.json())
             .then(val=>setData(val))
 
+        setLoading(false)
+            
     },[])
+
+    if (loading){
+        return <p>Loading...</p>
+    }
 
     return(
         <div id="box">
+            
             {
                 data.map((item)=>{
                     return <div className="card" key={item.id}>

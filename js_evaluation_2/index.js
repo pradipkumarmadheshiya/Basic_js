1da89b6df267cb65ada6b3b1aa94974be8dae034
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2025-04-01 14:13:11 +0530

    sort filter search applied

--- js_evaluation_2/index.js
+++ js_evaluation_2/index.js
@@ -1,12 +1,16 @@
 const user_box=document.getElementById("user_box")
+const filter_users=document.getElementById("filter_users")
 const sort_users=document.getElementById("sort_users")
+const searchPostInp=document.getElementById("searchPostInp")
+const searchBtn=document.getElementById("searchBtn")
 
 const url="https://jsonplaceholder.typicode.com/posts/"
 
 // Fetch Posts
+let data=[]
 async function fetchPosts(){
     const res=await fetch(url)
-    const data=await res.json()
+    data=await res.json()
     displayPosts(data)
 }
 fetchPosts()
@@ -33,9 +37,48 @@ function displayPosts(data){
     })
 }
 
+// filter posts
+function filterUsersFun(){
+    user_box.innerHTML=""
+    let filterPosts=[...data]
+    const filter_users_value=filter_users.value
+
+    if(filter_users_value===""){
+        displayPosts(data)
+    }
+    else{
+        const filteredUsers=filterPosts.filter((item)=>item.userId==filter_users_value)
+        displayPosts(filteredUsers)
+    }
+}
+filter_users.addEventListener("click", filterUsersFun)
+
 // sort posts
-function sortFun(){
+function sortPostsFun(){
+    let sortPost=[...data]
+    const sort_users_value=sort_users.value
     user_box.innerHTML=""
     
+    if (sort_users_value==="ascending"){
+        sortPost.sort((a,b)=>a.title.localeCompare(b.title))
+        displayPosts(sortPost)
+    }
+    else if(sort_users_value==="descending"){
+        sortPost.sort((a,b)=>b.title.localeCompare(a.title))
+        displayPosts(sortPost)
+    }
+    else{
+        displayPosts(data)
+    }
 }
-sort_users.addEventListener("click", sortFun)
+sort_users.addEventListener("click", sortPostsFun)
+
+// search post
+function searchPostFun(){
+    user_box.innerHTML=""
+    const searchPostInpValue=searchPostInp.value.trim().toLowerCase()
+    
+    const searchPost=data.filter((item)=>item.title.toLowerCase().includes(searchPostInpValue)  || item.body.toLowerCase().includes(searchPostInpValue))
+    displayPosts(searchPost)
+}
+searchBtn.addEventListener("click", searchPostFun)
